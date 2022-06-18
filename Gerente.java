32fcbe34a9f2a1067e389509e2c78e539644b18f
Marco Antonio da Silva Francisco <marcospatton@hotmail.com>
2022-06-18 12:07:35 -0300

    Add files via upload

--- Gerente.java
+++ Gerente.java
@@ -1,13 +1,25 @@
 
-public class Funcionario {
+public class Gerente extends Funcionario{
 
 	private String nome;
 	private String cpf;
 	private double salario;
+	private int senha;
 	
+	public void setSenha(int senha) {
+		this.senha = senha;
+	}
+	
+	public boolean autentica(int senha) {
+		if(this.senha == senha) {
+			return true;
+		} else {
+			return false;
+		}
+	}
 	
 	public double getBonificacao() {
-		return this.salario * 0.1;
+		return this.salario;
 	}
 	
 	public String getNome() {
--- TesteFuncionario.java
+++ TesteFuncionario.java
@@ -4,13 +4,12 @@ public class TesteFuncionario {
 	public static void main(String[] args) {
 		
 		Funcionario nico = new Funcionario();
-		nico.setNome("Nico Steppot");
-		nico.setCpf("271.223.658-08");
-		nico.setSalario(2600.00);
+		nico.setNome("nico steppat");
+		nico.setCpf("223355646-9");
+		nico.setSalario(2500.80);
 		
 		System.out.println(nico.getNome());
 		System.out.println(nico.getBonificacao());
-
 	}
 
 }
